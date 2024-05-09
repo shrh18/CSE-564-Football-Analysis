36017178277a51f6cc09f2a3f571ffdb4837a7b2
shrh18 <shreyas.habade18@gmail.com>
2024-05-08 21:48:09 -0400

    Added scatterplot and barplot

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -4,21 +4,18 @@ import EuropeMap from './EuropeMap';
 import * as d3 from 'd3'; // Import the d3 library
 import RadarPlot from './radarplot';
 import ScatterPlotMatrix from './ScatterplotMatrix';
+import Barplot from './Barplot.js';
 
 const Dashboard = () => {
     return (
         <div className="dashboard">
             <div className="map"><EuropeMap /></div>
             {/* <div className="empty"></div> */}
-            <div className="scatterPlot">Scatter Plot Matrix</div>
+            <div className="scatterPlot">Scatter Plot Matrix
+                 <ScatterPlotMatrix />
+            </div>
             <div className="graph">
-                <div className="tabs">
-                    <button>Tab 1</button>
-                    <button>Tab 2</button>
-                    <button>Tab 3</button>
-                </div>
-                <ScatterPlotMatrix />
-                <div className="graphContent">Graph Area</div>
+                <Barplot />
             </div>
             <div className="parallelCoordinatesPlot">
                 {/* Radar Plot
